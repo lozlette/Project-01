20810469bd8f8c36dff706a613acef060bb21ea8
Lauren Bell <laurengilbert0@gmail.com>
2019-01-03 19:05:43 +0000

    game board setup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const h1 = document.querySelector('h1')
-  h1.textContent = 'HelloWorld!'
+  const width = 8
+  const grid = document.querySelector('.grid')
+  let square
+
+  function boardSetup(){
+    for (let i=0; i<width*width; i++){
+      //create div in DOM
+      square = document.createElement('div')
+      //connect square variable with css class
+      square.className = 'square'
+      //append the square to the grid
+      grid.appendChild(square)
+    }
+  }
+
+  boardSetup()
 })
