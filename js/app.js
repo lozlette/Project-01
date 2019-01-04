7f5c5ff8adb74e436c551a1521038639c4945a09
Lauren Bell <laurengilbert0@gmail.com>
2019-01-04 11:32:38 +0000

    game board changed from JS setup to HTML in order to add multiple classes to some elements

--- js/app.js
+++ js/app.js
@@ -1,18 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const width = 8
-  const grid = document.querySelector('.grid')
-  let square
 
-  function boardSetup(){
-    for (let i=0; i<width*width; i++){
-      //create div in DOM
-      square = document.createElement('div')
-      //connect square variable with css class
-      square.className = 'square'
-      //append the square to the grid
-      grid.appendChild(square)
-    }
-  }
 
-  boardSetup()
+
+
 })
