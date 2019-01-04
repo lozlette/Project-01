986a5a0630e878299a19ed3e0dcc7051eefd41f6
Lauren Bell <laurengilbert0@gmail.com>
2019-01-04 13:14:08 +0000

    dice roll logic added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const dice = document.querySelector('.dice-button')
+  // const squareToMoveTo = document.querySelector(`[data-id=${number}]`)
+
+  let player1Position = 1
+  let player2Position = 1
+
+  function rollDice() {
+    //generate a random number between 1 and 6
+    const diceRoll = Math.floor(Math.random() * 6) + 1
+    console.log(diceRoll)
+    console.log(player1Position += diceRoll)
 
 
+    //Add diceRoll number to data-id square that player1 is on
 
+  }
+
+
+  dice.addEventListener('click', rollDice)
 })
