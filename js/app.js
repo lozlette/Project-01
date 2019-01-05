cb343392614f4df299f4dcd4a2af91bf61158c18
Lauren Bell <laurengilbert0@gmail.com>
2019-01-05 16:34:42 +0000

    logic added to move player on the correct no. of squares from top of ladder

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let player1Position = 0
   let player1NewPosition
   let topLadder1
+  let topLadder2
 
   // let player2Position = 1
 
@@ -15,23 +16,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     if (squareToMoveTo.classList.contains('ladder')){
       squareToMoveTo.classList.remove('ladder')
+      player1Position = 9
     }
   }
 
   //if squareToMoveTo is bottom of ladder1 move to top of ladder
   function ladder(){
-    if(squareToMoveTo.dataset.id === '7') {
-      //after a delay of 1.5 seconds remove player1Counter from that square
+    if (squareToMoveTo.dataset.id === '7') {
+      //after a delay of 1.5 seconds remove player1Counter from that square...
       setTimeout(() => {
         squareToMoveTo.classList.remove('player1Counter')
-        // add class to square with data-id of 9
+        // .. and then add player1Counter class to square with data-id of 9
         topLadder1 = document.querySelector(`[data-id="${9}"]`)
-        topLadder1.classList.add('player1Counter')
         removeSnakeOrLadderClass()
-        console.log(topLadder1)
+        topLadder1.classList.add('player1Counter')
+        console.log('You get to go up the ladder!')
+      }, 1500)
+    } else if (squareToMoveTo.dataset.id === '21') {
+      setTimeout(() => {
+        squareToMoveTo.classList.remove('player1Counter')
+        topLadder2 = document.querySelector(`[data-id="${27}"]`)
+        removeSnakeOrLadderClass()
+        topLadder1.classList.add('player1Counter')
+        console.log(topLadder2)
         console.log('You get to go up the ladder!')
       }, 1500)
     }
+
   }
 
   //remove counter from previous square
@@ -39,6 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
     squareToMoveTo.classList.remove('player1Counter')
     if (topLadder1.classList.contains('player1Counter')){
       topLadder1.classList.remove('player1Counter')
+      player1NewPosition = 9
     }
   }
 
@@ -48,12 +60,16 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(diceRoll)
     //Add diceRoll number to data-id square that player1 is on
     player1NewPosition = player1Position += diceRoll
+
+
+
     squareToMoveTo = document.querySelector(`[data-id="${player1NewPosition}"]`)
     //add class of player1 to squareToMoveTo
     squareToMoveTo.classList.add('player1Counter')
     removeSnakeOrLadderClass()
     ladder()
     console.log(squareToMoveTo)
+    console.log(player1NewPosition)
   }
 
   dice.addEventListener('mouseup', rollDice)
